066d72da3d3060f885dfd7669ff70c9c77cddc37
devyank <devyanknagpal2002@gmail.com>
2022-03-18 02:36:12 +0530

    vedio created

--- fron/src/Pages/Home.jsx
+++ fron/src/Pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+// import Player from '../component/Player'
+import v from '../component/v3.mp4'
 export default function Home() {
   return (
-    <div>Home</div>
+   <Container>
+
+<video loop autoPlay style={{width:'100%',height:'90vh'}}>
+        <source
+          src={v}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
+
+    
+   </Container> 
   )
 }
