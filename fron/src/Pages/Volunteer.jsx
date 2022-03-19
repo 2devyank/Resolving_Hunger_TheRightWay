1412af7cfbcbccfa056f9f59942443a12bef2dff
devyank <devyanknagpal2002@gmail.com>
2022-03-19 16:36:16 +0530

    volunteer

--- fron/src/Pages/Volunteer.jsx
+++ fron/src/Pages/Volunteer.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import {Container, Form} from 'react-bootstrap'
+import '../Styles/volun.css'
 export default function Volunteer() {
   return (
-    <div>Volunteer</div>
+    <Container className='v'>
+<br />
+      <div className='vol' >
+        <h1>Volunteer With Us</h1>
+        <p>Though our motive through 
+           this program is different we have a different way of approach in ending Hunger.
+           but small acts when multiplied by millions of people, can transform the world .
+          We Hereby request People to come and join us and 
+          provide help in whatever possible way they can . 
+        </p>
+      </div>
+      <div>
+        <div>
+          <div><Form></Form></div>
+          <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyrSu673Mf-aoF07CUjeMbS4bMUaFWlSjDOg&usqp=CAU" alt="" /></div>
+        </div>
+      </div>
+
+        </Container>
   )
 }
