02604a29a6fb0f3044476c21bd17df11d7efc0d7
devyank <devyanknagpal2002@gmail.com>
2022-03-21 00:47:10 +0530

    home

--- fron/src/Pages/Home.jsx
+++ fron/src/Pages/Home.jsx
@@ -94,7 +94,44 @@ nearly 500 grams of food is wasted — through preparation, spoilage and leftove
 <p>Around half a billion pounds' worth of edible food is thrown each year due to the sell by date set by the owner in reality no one knows if food is edible</p></div>
   </div>
 </div>
+<div className="sec">
+  <div>
+  <h1>Initiative Through This Program</h1>
+<p>Through this program we provide a platform for  restaurants owner's , grocer's ,wedding organizers etc
+through our platform they get two choices either they can directly connect with the Non Government Oganization  or they 
+can provide information about how and when they experience food wastage , Which could reach the deserving one.
+There are a lot of NGOs that are working to control food wastage by distributing excess food in areas where it is needed </p>
 
+  </div>
+  </div>
+  <div className="sec">
+  <div>
+  <h1>Reason For This Program</h1>
+<p>Indian weddings! There is no doubt that in Indian culture, weddings are the biggest event
+   and happen on a very large scale. People spend most of their life savings
+    in organizing lavish weddings of their kid(s) with the most scrumptious food. 
+    Food is one of the main and very important things about Indian weddings. 
+    Tons of food is prepared for the grand wedding celebrations, 
+    but does all that food get eaten? The answer to this is NO. 
+    In fact, all that food is thrown away and wasted.  </p>
+<p>60% of food thrown into trash by restaurants is due to calculateion errors , A guset leaves approximately 
+  17% of his/her plate without consuming ,4 to 10% of food purchased get wasted before it reaches the guset
+</p>
+<p>According to the USDA’s Food Safety and Inspection Service,
+  manufacturers put “best by” or “best if used by” dates on their products to 
+  let retail stores and consumers know how long their products are expected 
+  to maintain their best taste and texture.
+
+
+These dates aren’t required by federal law (though some states require them)
+ and don’t necessarily indicate a product’s safety (with the exception of baby formula). 
+ In fact, perishable products are usually safe to consume beyond their “best by” 
+ date if they’ve been handled and stored properly. </p>
+  
+
+  </div>
+
+</div>
     <div className='last'>
       <div className='end'>
        <h2>Make Food Reach The Right Place</h2><a href="/donor">
