e09a4a308c95d020a03c71ee9e082a35daa36482
devyank <devyanknagpal2002@gmail.com>
2022-03-20 21:15:08 +0530

    facts

--- fron/src/Pages/Home.jsx
+++ fron/src/Pages/Home.jsx
@@ -3,6 +3,9 @@ import { Button, Carousel, Container } from 'react-bootstrap'
 // import Player from '../component/Player'
 import v from '../component/v3.mp4'
 import svg from '../component/teamwork.mp4'
+import food from '../component/food.png'
+import chef from '../component/chef-platter.png'
+import trash from '../component/trash.png'
 import '../Styles/home.css'
 import pic from '../component/pic.png'
 import pic1 from '../component/pic1.jpg'
@@ -28,11 +31,11 @@ export default function Home() {
       className="d-block w-100"
       src={pic3}
       alt="First slide"
-      width='50%'
+      // width='50%'
     />
     <Carousel.Caption>
       {/* <h5>First slide label</h5> */}
-      <p>Food Needs To reach the right place.</p>
+      {/* <p>Food Needs To reach the right place.</p> */}
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
@@ -55,13 +58,14 @@ export default function Home() {
 </Carousel>
       {/* <img src={pic} alt="" style={{width:'100%',height:'82vh'}}/> */}
       </div>
+      <br />
 <div className='fir'>
   <div className='first'>
     <div className='l'><h2>Our Aim</h2>
     <p>To Bring Non Goverment Organization's and Wedding Organizers , Grocer , Restaurants
        Owner Under One Roof</p>
     </div>
-    <div className='r'>
+    {/* <div className='r'>
       <video loop autoPlay >
         <source
           src={svg}
@@ -69,9 +73,28 @@ export default function Home() {
         />
         Your browser does not support the video tag.
       </video>
-      </div>
+      </div> */}
   </div>
 </div>
+<div className='sec'>
+    <h1>Facts</h1>
+  <div className='facts'>
+<div className='graph'><img src={food} alt="" />
+<p>According to several types of research,
+ approximately 40 percent of the 
+food prepared at a wedding venue is wasted
+ and thrown away 
+after the wedding ends</p>
+</div>
+<div className='graph'> <img src={chef} alt="" />
+
+<p>For every meal eaten in a restaurant, 
+nearly 500 grams of food is wasted — through preparation, spoilage and leftovers on diners' plates.</p></div>
+<div className='graph'><img src={trash} alt="" />
+<p>Around half a billion pounds' worth of edible food is thrown each year due to the sell by date set by the owner in reality no one knows if food is edible</p></div>
+  </div>
+</div>
+
     <div className='last'>
       <div className='end'>
        <h2>Make Food Reach The Right Place</h2><a href="/donor">
